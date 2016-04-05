82d56236221c63020ce01810c37ca202053fff5f
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-04-05 20:01:05 +0200

    Update saving to hdfstore

--- scripts/timelapse_dataextraction.py
+++ scripts/timelapse_dataextraction.py
@@ -21,51 +21,75 @@ Options:
 
 '''
 
-import dragonboard
+import dragonboard as dr
 from tqdm import tqdm
 import os
 from docopt import docopt
 import pandas as pd
 from collections import defaultdict
+import numpy as np
 
 
-def f(x, a, m, b):
-    return a * x**m + b
+def write(store, data):
+    for (pixel, gain), value in data.items():
+        df = pd.DataFrame(value)
+        df['sample'] = df['sample'].astype('int16')
+        df['adc_counts'] = df['adc_counts'].astype('int16')
+        df['cell'] = df['cell'].astype('int16')
+        df['delta_t'] = df['delta_t'].astype('float32')
+
+        store.append(
+            'pixel_{}/{}'.format(pixel, gain),
+            df,
+        )
+
+
+def kill():
+    os.system('kill {}'.format(os.getpid()))
 
 
 def extract_data(inputfiles):
     ''' calculate time lapse dependence for a given capacitor '''
 
-    with pd.HDFStore('data.hdf5', mode='a') as store:
+    with pd.HDFStore('data.hdf5', mode='w', comp_level=5, comp_lib='blosc') as store:
+
+        sample_ids = None
         for filename in sorted(inputfiles):
-            try:
-                cell_id = 2424
-                gaintype = 'low'
-                pixelindex = 0
+            data = defaultdict(lambda: defaultdict(list))
 
-                data = defaultdict(list)
+            for event in tqdm(
+                    iterable=dr.EventGenerator(filename),
+                    desc=os.path.basename(filename),
+                    leave=True,
+                    unit=' events',
+                    ):
 
-                for event in tqdm(
-                        iterable=dragonboard.EventGenerator(filename),
-                        desc=os.path.basename(filename),
-                        leave=True,
-                        unit=' events',
-                        ):
+                if sample_ids is None or sample_ids.shape != event.roi:
+                    sample_ids = np.arange(event.roi)
 
-                    stop_cell = event.header.stop_cells[gaintype][pixelindex]
-                    if stop_cell <= cell_id < (stop_cell + event.roi):
-                        data['time'].append(event.header.counter_133MHz / 133e6)
-                        data['adc_counts'].append(
-                            event[2][pixelindex][gaintype][cell_id - stop_cell]
+                for pixel, pixel_data in enumerate(event.data):
+                    for gain in pixel_data.dtype.names:
+
+                        stop_cell = event.header.stop_cells[pixel][gain]
+
+                        delta_ts = event.since[pixel][gain]
+                        valid = np.logical_not(np.isnan(delta_ts))
+                        if not np.any(valid):
+                            continue
+
+                        data[(pixel, gain)]['delta_t'].extend(delta_ts[valid])
+                        data[(pixel, gain)]['cell'].extend(
+                            dr.sample2cell(sample_ids[valid], stop_cell)
                         )
-                        data['sample_id'].append(cell_id - stop_cell)
+                        data[(pixel, gain)]['sample'].extend(sample_ids[valid])
+                        data[(pixel, gain)]['adc_counts'].extend(pixel_data[gain][valid])
 
-                df = pd.DataFrame(data)
-                df['delta_t'] = df.time.diff()
-                store.append('cell{}/gain{}'.format(cell_id, gaintype), df)
+                if (event.header.event_counter + 1) % 10000 == 0:
 
-            except Exception as e:
-                print(e)
+                    write(store, data)
+                    data = defaultdict(lambda: defaultdict(list))
+
+            write(store, data)
 
 
 if __name__ == '__main__':
